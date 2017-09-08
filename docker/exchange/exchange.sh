7807084f92478efa8f291fb9cc4827a8401b3877
camerontesterman <camerontesterman@hotmail.com>
2017-09-08 10:58:55 -0500

    exchange-settings.sh fixes

--- docker/exchange/exchange.sh
+++ docker/exchange/exchange.sh
@@ -17,12 +17,13 @@ virtualenv $VENV_DIR
 chmod 755 $VENV_DIR
 
 # remove geonode from requirements.txt and install requirements.txt
-sed -i.bak "/egg=geonode/d" $EXCHANGE_DIR/requirements.txt && \
+cp $EXCHANGE_DIR/requirements.txt $EXCHANGE_DIR/req.txt
+sed -i.bak "/egg=geonode/d" $EXCHANGE_DIR/req.txt && \
 PATH="/opt/boundless/vendor/bin":"${PATH}"                 && \
-$VENV_DIR/bin/pip install -r $EXCHANGE_DIR/requirements.txt
+$VENV_DIR/bin/pip install -r $EXCHANGE_DIR/req.txt
 
 # move exchange-settings.sh file to correct locations
-mv /root/exchange-settings.sh /etc/profile.d
+cp /root/exchange-settings.sh /etc/profile.d
 
 # setup MapLoom
 rm -rf $VENV_DIR/lib/python2.7/site-packages/maploom/static/maploom                   && \
@@ -34,17 +35,17 @@ ln -s /mnt/maploom/build/maploom.html $VENV_DIR/lib/python2.7/site-packages/mapl
 find /mnt/exchange -type f -name "*.pyc" -delete
 
 # source variables
-source /etc/profile.d/settings.sh
+source /etc/profile.d/exchange-settings.sh
 source /etc/profile.d/vendor-libs.sh
 
 # install Geonode
 $VENV_DIR/bin/pip install -e $GEONODE_DIR
 
 # copy supervisord.conf, waitress.sh, celery-worker.sh, exchange-init to correct places
-cp supervisord.conf /etc
-cp waitress.sh $EXCHANGE_DIR
-cp celery-worker.sh $EXCHANGE_DIR
-cp exchange-init /etc/init.d/exchange
+cp /root/supervisord.conf /etc
+cp /root/waitress.sh $EXCHANGE_DIR
+cp /root/celery-worker.sh $EXCHANGE_DIR
+cp /root/exchange-init /etc/init.d/exchange
 
 # restart systemctl daemon
 systemctl daemon-reload
@@ -56,4 +57,4 @@ wait_for_pg "database"
 run_migrations
 
 # start up waitress and celery
-$VENV_DIR/supervisord
+$VENV_DIR/bin/supervisord
